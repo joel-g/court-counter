8a3f3e9f0edf20c071ceab7f50f711538d8f5742
Joel Guerra <joelaguerra1@gmail.com>
2017-12-04 10:56:19 -0800

    all 3 buttons working

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -2,19 +2,33 @@ package com.example.android.courtcounter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    int scoreTeamA = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        displayForTeamA(1);
+        displayForTeamA(scoreTeamA);
     }
 
-    public void giveA3points() {
-        displayForTeamA(3);
+    public void giveA3points(View view) {
+        scoreTeamA += 3;
+        displayForTeamA(scoreTeamA);
+    }
+
+    public void giveA2points(View view) {
+        scoreTeamA += 2;
+        displayForTeamA(scoreTeamA);
+    }
+
+    public void giveA1points(View view) {
+        scoreTeamA += 1;
+        displayForTeamA(scoreTeamA);
     }
 
     public void displayForTeamA(int score) {
@@ -23,4 +37,3 @@ public class MainActivity extends AppCompatActivity {
     }
 
 }
-system

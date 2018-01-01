d87b0f02f09ad3cd5262c94390530635aa8623b1
joel-g <joelaguerra1@gmail.com>
2018-01-01 15:49:00 -0800

    write java functions for team b

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -8,6 +8,7 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     int scoreTeamA = 0;
+    int scoreTeamB = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,9 +22,19 @@ public class MainActivity extends AppCompatActivity {
         displayForTeamA(scoreTeamA);
     }
 
+    public void giveB3points(View view) {
+        scoreTeamB += 3;
+        displayForTeamB(scoreTeamB);
+    }
+
     public void giveA2points(View view) {
         scoreTeamA += 2;
         displayForTeamA(scoreTeamA);
+    }
+
+    public void giveB2points(View view) {
+        scoreTeamB += 2;
+        displayForTeamB(scoreTeamB);
     }
 
     public void giveA1points(View view) {
@@ -31,8 +42,18 @@ public class MainActivity extends AppCompatActivity {
         displayForTeamA(scoreTeamA);
     }
 
+    public void giveB1points(View view) {
+        scoreTeamB += 1;
+        displayForTeamB(scoreTeamB);
+    }
+
     public void displayForTeamA(int score) {
         TextView scoreView = (TextView) findViewById (R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    public void displayForTeamB(int score) {
+        TextView scoreView = (TextView) findViewById (R.id.team_b_score);
         scoreView.setText(String.valueOf(score));
     }
 
